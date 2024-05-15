cd7e2559a0b077cfdc95e685dd68de30c5c60c07
cico84 <filippo.cichocki@enea.it>
2024-05-15 12:42:38 +0200

    Added run_code script and sbatch template

---
+++ bin/run_code_g100.sh
@@ -0,0 +1,10 @@
+# Load required modules for compilation
+module purge
+module load autoload intel-oneapi-compilers/2023.2.1
+
+# Compile code, if necessary
+make clean -f "${HTHETA}/src/makefile.txt"
+make HTHETA -f "${HTHETA}/src/makefile.txt"
+
+# Launch sbatch executable (a file named "sbatch_sim_name.sh" must be available in the bin folder)
+sbatch "sbatch.sh"
---
+++ bin/sbatch_template.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --nodes=1                    # nodes
+#SBATCH --ntasks-per-node=1          # tasks per node
+#SBATCH --cpus-per-task=1            # cores per task
+#SBATCH --gres=gpu:1                 # GPUs per node
+#SBATCH --mem=494000                 # mem per node (MB)
+#SBATCH --time=24:00:00              # time limit (d-hh:mm:ss)
+#SBATCH --account=tra24_openhack_0   # account ($ saldo -b)
+#SBATCH --partition=boost_fua_prod   # partition name
+#SBATCH --qos=normal                 # quality of service
+
+module purge
+module load autoload intel-oneapi-compilers/2023.2.1
+
+export KMP_AFFINITY=compact
+# export KMP_AFFINITY=verbose,compact
+# export KMP_AFFINITY="verbose,granularity=core,compact"
+# export KMP_STACKSIZE=64m
+
+export OMP_STACKSIZE=1G
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# export OMP_PLACES=cores
+# export OMP_PROC_BIND=true
+
+srun ./htheta.exe
