abd56e8b171762fd743cda9f0d520ad7eeb9a1e1
cico84 <filippo.cichocki@enea.it>
2024-05-21 12:26:12 +0200

    Minor

--- bin/sbatch_template.sh
+++ bin/sbatch_template.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1            # cores per task
 #SBATCH --gres=gpu:1                 # GPUs per node
 #SBATCH --mem=494000                 # mem per node (MB)
-#SBATCH --time=24:00:00              # time limit (d-hh:mm:ss)
+#SBATCH --time=00:20:00              # time limit (d-hh:mm:ss)
 #SBATCH --account=tra24_openhack     # account ($ saldo -b)
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --reservation=s_tra_hack
