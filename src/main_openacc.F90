2bdd30db7ec5b0ca412f5e88f4a4368ba686347c
cico84 <filippo.cichocki@enea.it>
2024-05-21 12:40:24 +0200

    Minor

--- src/main_openacc.F90
+++ src/main_openacc.F90
@@ -322,19 +322,21 @@
                   call nvtxEndRange
                   ! Copy data from GPU to CPU memory
 
-                  ! Compute Poisson's equation source term
-                  do j = 0, ny
-                        dpoi(j) = -(rhoi(j)-rhoe(j)) * dy**2 / eps0
-                  end do
-            
-                  !$acc enter data copyin( phi(0:ny), dpoi(0:ny), Ey(0:ny), y(0:ny) )
+                  !$acc enter data copyin( rhoi(0:ny), rhoe(0:ny), phi(0:ny), dpoi(0:ny), Ey(0:ny), y(0:ny) )
                   !$acc enter data copyin(  jpe(1:npe),  wye(1:npe), eype(1:npe) )
                   !$acc enter data copyin( vzpe(1:npe), vype(1:npe), vxpe(1:npe), ype(1:npe), zpe(1:npe) )
                   !$acc enter data copyin(        jpi(1:npi),        wyi(1:npi), eypi(1:npi) )
                   !$acc enter data copyin( vzpi(1:npi), vypi(1:npi), vxpi(1:npi), ypi(1:npi), zpi(1:npi) )
 
+                  ! Compute Poisson's equation source term
+                  !$acc parallel loop
+                  do j = 0, ny
+                        dpoi(j) = -(rhoi(j)-rhoe(j)) * dy**2 / eps0
+                  end do
+            
                   call nvtxStartRange('FIELD SOLVE')
                   ! Solve for the self-consistent azimuthal electric field
+                  !$acc update host(dpoi)
                   call fieldsolve
                   call nvtxEndRange
 
@@ -722,7 +724,7 @@
             Ey(ny)= Ey(0)
             !$acc end serial
             !$acc wait
-            
+
             !******************************************************************************
       
             return
@@ -751,8 +753,6 @@
             duektimi = -2.*kB*Ti0/Mi   ! Ions leap frog constant
 
             ! Electrons loop
-
-
             !$acc kernels loop
             do i = 1, npe
                   Eype(i) = wye(i)*Ey(jpe(i)-1) + (1.-wye(i))*Ey(jpe(i))
@@ -800,8 +800,8 @@
                   !   end if
             end do
     
+            ! Ions loop
             !$acc kernels loop
-            ! Ions loop   
             do i = 1, npi
                   Eypi(i) = wyi (i) * Ey(jpi(i) - 1) + (1. - wyi(i)) * Ey(jpi(i))
                   vypi(i) = vypi(i) + consti*Eypi(i)
