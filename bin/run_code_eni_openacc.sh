680f6b5bf3c5415a2e85aee5dd9b87fdda5821bf
cico84 <filippo.cichocki@istp.cnr.it>
2024-06-14 13:47:41 +0200

    Added script to run HTheta on ENI

---
+++ bin/run_code_eni_openacc.sh
@@ -0,0 +1,9 @@
+# Load required modules for compilation
+module load autoload petsc/3.15_dp_hypre_omp--hpc-sdk--20.11--binary
+
+# Compile code, if necessary
+make clean -f "${HTHETA}/src/makefile_openacc_eni.txt"
+make HTHETA -f "${HTHETA}/src/makefile_openacc_eni.txt"
+
+# Launch sbatch executable (a file named "sbatch_sim_name.sh" must be available in the bin folder)
+qsub "qsub.sh"
